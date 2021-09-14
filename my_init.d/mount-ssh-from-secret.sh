d1cc29d30183d7261db4e6ceb493f818e80c9523
Simon Adorf <simon.adorf@epfl.ch>
2021-09-14 17:54:39 +0200

    Copy the ssh key file instead of linking to it.

--- my_init.d/mount-ssh-from-secret.sh
+++ my_init.d/mount-ssh-from-secret.sh
@@ -4,7 +4,7 @@ if [ ! -d /home/${SYSTEM_USER}/.ssh/ ] && [ -f /run/secrets/ssh_key ]; then
   mkdir --mode=0700 /home/${SYSTEM_USER}/.ssh/
   touch /home/${SYSTEM_USER}/.ssh/known_hosts
   # Linking ssh key from secret.
-  ln -s /run/secrets/ssh_key /home/${SYSTEM_USER}/.ssh/id_rsa
+  cp /run/secrets/ssh_key /home/${SYSTEM_USER}/.ssh/id_rsa
 
   # Try to fix permissions (should fail when user was not yet created).
   chown -R ${SYSTEM_USER}:${SYSTEM_USER} /home/${SYSTEM_USER}/.ssh || echo "initial startup"
