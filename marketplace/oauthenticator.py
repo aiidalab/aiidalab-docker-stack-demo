5604ab37744729aac58c9e6e7974a96315780dff
jusong.yu <jusong.yeu@gmail.com>
2021-10-21 17:47:06 +0200

    Store marketplace access token as env per user

--- marketplace/oauthenticator.py
+++ marketplace/oauthenticator.py
@@ -3,6 +3,25 @@ from oauthenticator.generic import GenericOAuthenticator
 
 from tornado.httpclient import HTTPRequest, AsyncHTTPClient
 
+from jupyterhub.handlers.base import BaseHandler
+
+# add route for terms & conditions
+class HeartbeatHandler(BaseHandler):
+
+    def get(self): # pylint: disable=arguments-differ
+        """heartbeat"""
+        self.write("exist is pain.")
+
+        return
+    
+class LoginHandler(BaseHandler):
+
+    def get(self): # pylint: disable=arguments-differ
+        """heartbeat"""
+        self.write("login by me.")
+
+        return
+
 class MarketplaceOAuthenticator(GenericOAuthenticator):
     """A GenericOAuthenticator with assigned LoginHandler containing Marketplace specific urls"""
 
@@ -35,7 +54,6 @@ class MarketplaceOAuthenticator(GenericOAuthenticator):
 
         # Get full user details
         access_token = resp_json['auth_state']['access_token']
-        refresh_token = resp_json['auth_state']['refresh_token']
         user_json = await self._fetch_user_details(access_token)
 
         # Update real username and store additional details in in auth_state
@@ -43,3 +61,12 @@ class MarketplaceOAuthenticator(GenericOAuthenticator):
         resp_json['auth_state']['oauth_user'].update(user_json)
 
         return resp_json
+
+    async def pre_spawn_start(self, user, spawner):
+        """Pass upstream_token to spawner via environment variable"""
+        auth_state = await user.get_auth_state()
+        if not auth_state:
+            # auth_state not enabled
+            return
+        spawner.environment['MP_ACCESS_TOKEN'] = auth_state['access_token']
+        spawner.environment['MP_REFRESH_TOKEN'] = auth_state['refresh_token']
