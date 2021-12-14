099c9b4a95838db38256a292c2c6b1b327f48387
Jason.Yu <jusong.yeu@gmail.com>
2021-12-14 13:42:03 +0100

    Marketplace deploy: store access_token as env variable during spawner process (#20)
    * Inject marketplace access token as environment variable into user environment
    * Add a URL handler for terms and conditions

--- marketplace/oauthenticator.py
+++ marketplace/oauthenticator.py
@@ -3,6 +3,17 @@ from oauthenticator.generic import GenericOAuthenticator
 
 from tornado.httpclient import HTTPRequest, AsyncHTTPClient
 
+from jupyterhub.handlers.base import BaseHandler
+
+# add route for terms & conditions
+class TermsConditionsHandler(BaseHandler):
+
+    def get(self): # pylint: disable=arguments-differ
+        """Terms and conditions"""
+        # Note: This placeholder serves a demonstration for adding
+        # additional routes and associated handlers.
+        self.write("This is the terms and conditions of this service.")
+    
 class MarketplaceOAuthenticator(GenericOAuthenticator):
     """A GenericOAuthenticator with assigned LoginHandler containing Marketplace specific urls"""
 
@@ -35,7 +46,6 @@ class MarketplaceOAuthenticator(GenericOAuthenticator):
 
         # Get full user details
         access_token = resp_json['auth_state']['access_token']
-        refresh_token = resp_json['auth_state']['refresh_token']
         user_json = await self._fetch_user_details(access_token)
 
         # Update real username and store additional details in in auth_state
@@ -43,3 +53,12 @@ class MarketplaceOAuthenticator(GenericOAuthenticator):
         resp_json['auth_state']['oauth_user'].update(user_json)
 
         return resp_json
+
+    async def pre_spawn_start(self, user, spawner):
+        """Pass upstream_token to spawner via environment variable"""
+        auth_state = await user.get_auth_state()
+        if not auth_state:
+            # auth_state not enabled
+            return
+        spawner.environment['MP_ACCESS_TOKEN'] = auth_state['access_token']
+        spawner.environment['MP_REFRESH_TOKEN'] = auth_state['refresh_token']
