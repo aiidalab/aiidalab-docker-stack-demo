69d8c6e79d2e2ecd3c03a6e640e5d9b234501f2f
Simon Adorf <carl.simon.adorf@gmail.com>
2022-02-22 13:25:26 +0100

    Deploy for www.materials-marketplace.eu.

--- marketplace/oauthenticator.py
+++ marketplace/oauthenticator.py
@@ -17,43 +17,6 @@ class TermsConditionsHandler(BaseHandler):
 class MarketplaceOAuthenticator(GenericOAuthenticator):
     """A GenericOAuthenticator with assigned LoginHandler containing Marketplace specific urls"""
 
-    async def _fetch_user_details(self, access_token):
-        """Get user details from Marketplace user-service API from user ID"""
-        import json
-
-        http_client = AsyncHTTPClient()
-        headers = {
-            "Accept": "application/json",
-            "User-Agent": "JupyterHub",
-            "Authorization": f"Bearer {access_token}",
-        }
-
-        # Use GET request method
-        req = HTTPRequest(
-            "https://{{ marketplace_host }}/user-service/userinfo",
-            method="GET",
-            headers=headers,
-            validate_cert=False,
-        )
-        resp = await http_client.fetch(req)
-        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
-
-        return resp_json
-
-    async def authenticate(self, handler, data=None):
-        """Overloads authentication service to include an additional call to marketplace user-service API."""
-        resp_json = await super().authenticate(handler, data=data)
-
-        # Get full user details
-        access_token = resp_json['auth_state']['access_token']
-        user_json = await self._fetch_user_details(access_token)
-
-        # Update real username and store additional details in in auth_state
-        resp_json['name'] = user_json['name']
-        resp_json['auth_state']['oauth_user'].update(user_json)
-
-        return resp_json
-
     async def pre_spawn_start(self, user, spawner):
         """Pass upstream_token to spawner via environment variable"""
         auth_state = await user.get_auth_state()
