919dec946083770a24fd8f56e1f0a7423dafc1d5
Simon Adorf <carl.simon.adorf@gmail.com>
2022-02-23 12:44:08 +0100

    Use base image.

--- my_init.d/prepare_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -em
-
-su "${SYSTEM_USER}" -c /opt/install-widget_bandsplot.sh
-su "${SYSTEM_USER}" -c /opt/setup-quantum-espresso-code.sh
-
-# Start background process to install the SSSP pseudo potentials:
-su "${SYSTEM_USER}" -c /opt/install-sssp &
