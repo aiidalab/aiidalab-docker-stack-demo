251103f1d4645c4a02dc6e17adb0efc9e0bb44f4
Simon Adorf <carl.simon.adorf@gmail.com>
2022-02-23 12:44:08 +0100

    Use base image.

--- opt/install-widget_bandsplot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -u
-
-# Manually install and enable widget_bandsplot (not clear why this is necessary)
-/opt/conda/bin/python -m pip install widget_bandsplot
-jupyter nbextension enable --py widget_bandsplot
